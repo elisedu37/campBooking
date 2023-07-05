27a790a9df7cc91bd4058b9f15774e22a7a8c3bd
elisedu37 <46353414+elisedu37@users.noreply.github.com>
2023-07-05 09:51:38 +0200

    Update components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+/**
+ * Composant pour le footer
+ * @returns {JSX}
+ */
 const Footer = () => {
   return <div className="px-4">© 2023 - Elise GIGOT. All Rights Reserved.</div>;
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import React from 'react';
+// Composants
 import Navbar from './Header/Navbar';
 import Banner from './Header/Banner';
 
+/**
+ * Composant pour le header : barre de navigation et banner
+ * @param {function} setSort setter pour les filtres
+ * @returns {JSX}
+ */
 const Header = ({ setSort }) => {
   return (
     <header className="flex flex-col">
